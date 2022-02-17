ea1fd7ddb601565f854d6906bcb688bf4132bb7a
Viqas Hussain <viqashussain@hotmail.co.uk>
2022-02-17 11:18:35 +0000

    fix the heights of the coins

--- matter-objects/GoldCoin.tsx
+++ matter-objects/GoldCoin.tsx
@@ -9,7 +9,7 @@ interface Props {
 export default class GoldCoin extends Component<Props, {}> {
     render() {
         const width = this.props.size[0];
-        const height = this.props.size[1];
+        const height = this.props.size[1] * 0.66;
         const x = this.props.body.position.x - width / 2;
         const y = this.props.body.position.y - height / 2;
 
